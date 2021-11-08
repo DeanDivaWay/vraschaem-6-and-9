6424758c979f7628a4a4b315bc04f6858b57c57f
DeanDivaWay <92186059+DeanDivaWay@users.noreply.github.com>
2021-11-08 13:08:13 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+def rot(num):
+
+    numberies__to_replace_count = str(num)
+    resultat__num = int(numberies__to_replace_count.replace('6', '9', 1))
+
+
+    print("Результат свой впишите: \n \n \n", resultat__num)
